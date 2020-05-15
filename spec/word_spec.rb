5378fdf7818324af249afbb500225d09df08b143
jhell85 <jhell85@gmail.com>
2020-05-15 11:24:12 -0700

    added failing test for .all method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,13 +4,18 @@ require ('rspec')
 
 describe "#Word" do
   
-  describe('#save') do
-    it('saves a word') do
+  describe '#save' do
+    it 'saves a word' do
       word = Word.new({ :name => "banana", :id => nil})
       word.save
       word2 = Word.new({:name => "Ruby", :id => nil})
       word2.save
       expect(Word.all).to(eq([word, word2]))
+    end
+  end
+  describe('.all') do
+    it 'returns an empty array when there are no words' do
+      expect(Word.all).to(eq([]))
     end
   end
 end
