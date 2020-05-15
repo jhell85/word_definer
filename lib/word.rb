7195d15d5d8a77f1079086c88ebb045b3f9edb6d
jhell85 <jhell85@gmail.com>
2020-05-15 10:43:25 -0700

    add, all and == methods added to Word class first test now passing

--- lib/word.rb
+++ lib/word.rb
@@ -1,5 +1,6 @@
 require ('pry')
 class Word
+  attr_reader :id
   attr_accessor :name
   @@words = {}
   @@total_rows = 0
@@ -7,5 +8,17 @@ class Word
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values
+  end
+
+  def save
+    @@words[self.id] = Word.new({:name => self.name, :id => self.id})
+  end
+
+  def ==(word_to_compare)
+    self.name == word_to_compare.name
   end
 end
