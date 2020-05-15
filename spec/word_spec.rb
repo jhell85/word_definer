cb5647c1a5afd66c5663fc565a4771dd432bec2a
jhell85 <jhell85@gmail.com>
2020-05-15 10:04:30 -0700

    first written test failing

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,5 +4,13 @@ require ('rspec')
 
 describe "#Word" do
   
-  
+  describe('#save') do
+    it('saves a word') do
+      word = Word.new({ :name => "banana", :id => nil})
+      word.save
+      word2 = Word.new({:name => "Ruby", :id => nil})
+      word2.save
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
 end
